01194f86577e4ce262346947e18f8a177de721db
neethutk08 <39289744+neethutk08@users.noreply.github.com>
2024-08-12 22:23:38 +0530

    Update streamlit_app.py
    adding fv_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,5 @@ if ingradients_list:
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json)
+fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
